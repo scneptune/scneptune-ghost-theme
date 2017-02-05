e9c727617e005af011ace07126b38e7a8c613f56
scneptune <rmalcome@gmail.com>
2017-02-05 12:36:32 -0800

    asset building process complete.

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,48 @@ const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const DashboardPlugin = require('webpack-dashboard/plugin');
-
+const autoprefixer = require('autoprefixer');
+const precss = require('precss');
+const postScss= require('postcss-scss')
 const nodeEnv = process.env.NODE_ENV || 'development'
 const isProduction = nodeEnv === 'production';
 
+const compiledCss = new ExtractTextPlugin({
+  filename: '/css/[name].css',
+  disable:false,
+  allChunks: true
+})
 
 const webpackPlugins = [
+
   new webpack.NamedModulesPlugin(),
   new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: JSON.stringify(nodeEnv)
     }
   }),
+  new webpack.NoEmitOnErrorsPlugin(),
   new webpack.optimize.CommonsChunkPlugin({
     names: ['vendor_a', 'vendor_b'],
-    filename: '[name].js',
+    filename: 'js/[name].js',
     minChunks: Infinity
-  })
+  }),
+  new webpack.LoaderOptionsPlugin({
+    debug: true,
+    options: {
+      postcss: [
+        precss(),
+        autoprefixer({
+          browsers: [
+            'last 3 version',
+            'ie >= 10',
+          ],
+        })
+      ],
+      context: path.join(__dirname, './src/'),
+    },
+  }),
+
 ]
 
 if (isProduction) {
@@ -39,7 +64,8 @@ if (isProduction) {
       output: {
         comments: true
       }
-    })
+    }),
+    compiledCss
   )
 } else  {
   webpackPlugins.push(
@@ -50,26 +76,30 @@ if (isProduction) {
 
 module.exports = {
   devtool: isProduction ? 'eval' : 'source-map',
-  context: path.resolve(__dirname, './src/js'),
-  entry: {
-    main: './main.js',
-    vendor_a: [
-      'babel-polyfill',
-      'redux-thunk',
-      'redux',
-    ],
-    vendor_b: [
-      'react',
-      'react-dom'
-    ]
-  },
+  context: path.resolve(__dirname, './src/'),
+  entry: ['./js/main.js',
+     './css/main.css',
+    // {
+    //   vendor_a: [
+    //     'babel-polyfill',
+    //     'redux-thunk',
+    //     'redux',
+    //   ]
+    // },
+    // {
+    //   vendor_b: [
+    //     'react',
+    //     'react-dom'
+    //   ]
+    // }
+  ],
   output: {
-    path: path.resolve(__dirname, './assets/js'),
+    path: path.resolve(__dirname, './assets/'),
     publicPath: '/',
-    filename: '[name].js',
+    filename: '/js/[name].js',
   },
   resolve: {
-    extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+    extensions: ['.js', '.jsx'],
     modules: [
       path.resolve(__dirname, 'node_modules'),
       path.resolve(__dirname, './src/js'),
@@ -84,6 +114,14 @@ module.exports = {
       }, {
         test: /\.(jpe?g|gif|png|svg)$/i,
         use: 'file-loader?name=/image/[name].[ext]'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use:  isProduction ? ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader?modules=false&importLoaders=1!postcss-loader?syntax=postcss-scss',
+        }) : ['style-loader', 'css-loader?modules=false&importLoaders=1!postcss-loader?syntax=postcss-scss'],
       }
     ]
   },
